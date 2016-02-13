38ee469fccb0553affe69cb0d15bd83800dd845d
horie1024 <ryosuke.pic@gmail.com>
2016-02-13 21:30:03 +0900

    oauthでcodeを取得してからaccess_tokenを取得するフローを追加

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,29 +1,14 @@
 var express = require('express'),
-  router = express.Router();
+  router = express.Router(),
+  config = require('../config/config');
 
 router.get('/', function(req, res, next) {
-
-/*
-  var options = {
-    url: 'https://graph.facebook.com/oauth/device',
-    form: {
-      type: 'device_code',
-      client_id: '1526483817646843',
-      scope: 'public_profile,user_likes'
-    }
-  };
-
-  request.post(options, function(error, response, body) {
-      console.log(response);
-  });
-  */
 
   res.redirect("https://www.facebook.com/dialog/oauth?client_id="
   + config.facebook.appId
   + "&redirect_uri=http://tk2-244-31758.vs.sakura.ne.jp:3000/redirect"
   + "&scope=");
 
-  //res.send('oauth test');
 });
 
 module.exports = router;
--- routes/redirect.js
+++ routes/redirect.js
@@ -9,11 +9,28 @@ router.get('/', function(req, res, next) {
 
   console.log(req.query.access_token);
 
-  if (typeof req.query.access_token !== 'undefined') {
-    FB.setAccessToken();
-  }
+  if (typeof req.query.code !== 'undefined') {
 
-  res.send('success');
+    var code = req.quert.code,
+      url = 'https://graph.facebook.com/v2.3/oauth/access_token?'
+      + 'client_id='
+      + config.facebook.appId
+      + '&redirect_uri=http://tk2-244-31758.vs.sakura.ne.jp:3000/redirect'
+      + '&client_secret=' + config.facebook.appSecret
+      + '&code=' + code;
+
+      res.redirect(url);
+
+  } else if (typeof req.query.access_token !== 'undefined') {
+
+    FB.setAccessToken(req.query.access_token);
+
+    console.log(req.query.access_token);
+
+    res.send('success');
+  } else {
+    // error
+  }
 });
 
 module.exports = router;
