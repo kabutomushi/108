aa149cf451f8db674ca39285db44d2136c771c3a
catto <catto.je1mji@gmail.com>
2016-02-13 10:12:45 +0900

    add config skelton

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.export = {
+    facebook: {
+        appId: "REPLACE",
+        appSecret: "REPLACE"
+    },
+    oxford: {
+        primaryKey: "REPLACE",
+        secondaryKey: "REPLACE"
+    }
+};
